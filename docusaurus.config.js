91bbd6775123f3d0990d5592a409144573380ab8
Sui <suihsilan@gmail.com>
2023-10-02 12:44:15 +1100

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           // },
           // theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          // customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
