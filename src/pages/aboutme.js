b302fa138051bf08b65600110a2daaa6c5466b50
Sui <suihsilan@gmail.com>
2023-10-02 12:16:45 +1100

    first commit

---
+++ src/pages/aboutme.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "@theme/Layout";
+const test = () => {
+  return (
+    <Layout>
+      <div>關於我</div>
+    </Layout>
+  );
+};
+
+export default test;
